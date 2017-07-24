5e722c510bc14b797b2d0189605d5fd84f99a60f
marioharper <harpmari12@gmail.com>
2017-07-24 08:49:41 -0500

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public Text scoreText;
+	public Text winText;
+	public Text pointText;
+
+	private int score = 0;
+	private int pointsToDisplay = 0;
+	private IEnumerator displayPointsCoroutine;
+
+	/* PRIVATE */
+	void Awake() {
+		if (instance == null) {
+			instance = this;
+		} else if (instance == this) {
+			Destroy (gameObject);
+		}
+
+		DontDestroyOnLoad (gameObject);
+		InitGame ();
+	}
+
+	void InitGame () {
+		scoreText.text = "Score: ";
+		winText.text = "";
+		pointText.text = "";
+	}
+
+	void UpdateCountText() {
+		scoreText.text = "Score: " + score.ToString ();
+
+		if (score >= 12) {
+			winText.text = "You Win!";
+		}
+	}
+
+	IEnumerator DisplayPoints (int points) {
+		if (points > 0 && pointsToDisplay > 0 || points < 0 && pointsToDisplay < 0) {
+			pointsToDisplay += points;
+		} else {
+			pointsToDisplay = points;
+		}
+
+		pointText.text = pointsToDisplay.ToString();
+
+		if (pointsToDisplay > 0) {
+			pointText.color = Color.green;
+		} else {
+			pointText.color = Color.red;
+		}
+		
+		yield return new WaitForSeconds(2);
+
+		pointText.text = "";
+		pointsToDisplay = 0;
+	}
+		
+	/* PUBLIC */
+	public void AddPoints(int points) {
+		if (displayPointsCoroutine != null) {
+			StopCoroutine (displayPointsCoroutine);
+		}
+
+		displayPointsCoroutine = DisplayPoints (points);
+
+		StartCoroutine(displayPointsCoroutine);
+
+		score += points;	
+		UpdateCountText ();
+	}
+
+	public int GetScore() {
+		return score;
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+		float moveJump = 0;
+
+		if (Input.GetButtonDown ("Jump") && gameObject.transform.position.y == .5) {
+			moveJump = 20;
+		}
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveJump, moveVertical);
+
+        rb.AddForce (movement * speed);
+    }
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			GameManager.instance.AddPoints (1);
+		} else if (other.gameObject.CompareTag ("PwrSpeed")) {
+			other.gameObject.SetActive (false);
+			speed *= 2;
+		}
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.CompareTag ("Wall")) {
+			GameManager.instance.AddPoints (-1);
+		}
+	}
+}
